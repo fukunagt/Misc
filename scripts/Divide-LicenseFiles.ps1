4d6a6d9377b171fa1702ef39550e70b4ab38936b
fukunagt <t-fukunaga@nec.com>
2023-05-10 16:27:31 +0900

    Modify the script.

--- scripts/Divide-LicenseFiles.ps1
+++ scripts/Divide-LicenseFiles.ps1
@@ -1,6 +1,7 @@
+$str = $Args[0].Substring(2, 5)
 $csv = import-csv $Args[0] -header "Serial","Key","Product","Code" -Encoding Default
 foreach ($csv in $csv)
 {
-    $dest = ".\" + $csv.Serial + ".key"
+    $dest = ".\" + $str + $csv.Serial + ".key"
     Write-Output ($csv.Serial + "," + $csv.Key + "," + $csv.Product + "," + $csv.Code + ",") | Out-File $dest -Encoding default
 }
